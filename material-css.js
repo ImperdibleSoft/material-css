78fbeee39993b0d69f3b8e9e2b37887b137cebc6
Rafael Pérez García <info@imperdiblesoft.com>
2015-07-14 15:07:53 -0300

    New animations

--- material-css.js
+++ material-css.js
@@ -620,6 +620,37 @@ $(document).ready(function(mc){
 			}
 		}
 		
+		else if( $(e.target).hasClass("mc-clickable") || $(e.target).hasClass("mc-button") || $(e.target).hasClass("mc-tab") || $(e.target).hasClass("mc-nav-item") || $(e.target).hasClass("mc-nav-subitem") ){
+			
+			/*	Click effect	*/
+			var parent, ink, d, x, y;
+			parent = $(e.target);
+			
+			//create .ink element if it doesn't exist
+			if(parent.find(".ink").length == 0){
+				parent.prepend("<span class='ink'></span>");
+			}
+
+			ink = parent.find(".ink");
+			//incase of quick double clicks stop the previous animation
+			ink.removeClass("animate");
+
+			//set size of .ink
+			if(!ink.height() && !ink.width()){
+				//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
+				d = Math.max(parent.outerWidth(), parent.outerHeight());
+				ink.css({height: d, width: d});
+			}
+
+			//get click coordinates
+			//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
+			x = e.pageX - parent.offset().left - ink.width()/2;
+			y = e.pageY - parent.offset().top - ink.height()/2;
+
+			//set the position and add class .animate
+			ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+		}
+		
 		mc.resizeLayoutImages();
 		mc.resizeDialogsHeight();
 		mc.updateFloatingButtons();
@@ -660,6 +691,7 @@ $(document).ready(function(mc){
 		/*	Remove the splash screen	*/
 		$("body").removeClass("mc-noscroll");
 		$(".mc-splash").fadeOut();
+			
 	});
 	
 	/*	Detect the window scroll	*/
